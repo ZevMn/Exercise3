4c5e65621e261950b520e2ea6cc29a1ef2091076
Zev Menachemson <zm1224@imperial.ac.uk>
2024-11-04 14:54:03 +0000

    Experimenting with Github API

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+import requests
+
 
 @app.route("/")
 def hello_world():
@@ -15,3 +17,19 @@ def submit():
     years_since_graduation = 2024 - int(input_year)
     return render_template("hello.html", name=input_name, age=input_age,
                            years=years_since_graduation)
+
+
+def make_request(username):
+    response = requests.get(f"https://api.github.com/users/{username}/repos")
+    if response.status_code == 200:
+        repos = response.json()
+        for repo in repos:
+            print(repo["full_name"])
+
+@app.route("/githubapi", methods=["GET", "POST"])
+def githubapi():
+    if request.method == "POST":
+        username = request.form.get("ghubusername")
+        make_request(username)
+        return render_template("githubapi.html", username=username)
+    return render_template("githubapi.html")
