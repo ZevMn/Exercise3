6041c1782d05e114b1092f4da0c0f072e95a4ee1
Zev Menachemson <zm1224@imperial.ac.uk>
2024-11-04 15:57:16 +0000

    done

--- app.py
+++ app.py
@@ -23,13 +23,30 @@ def make_request(username):
     response = requests.get(f"https://api.github.com/users/{username}/repos")
     if response.status_code == 200:
         repos = response.json()
-        for repo in repos:
-            print(repo["full_name"])
+        print(repos[0].keys())
+        return repos
+    return []
+    
+def make_commit_request(commit_url):
+    response = requests.get(commit_url)
+    if response.status_code == 200:
+        commit_data = response.json()[0]
+    return commit_data
+    
+def format_result(data):
+    result = []
+    for row in data:
+        repo_name = row["full_name"].split("/")[1]
+        commit_data = make_commit_request(row["commits_url"].replace("{/sha}", ""))
+        new_dict = {"repo_name": repo_name, "last_updated": row["updated_at"], "commits_url": row["git_commits_url"], "hash": commit_data["sha"], "author": commit_data["commit"]["author"]["name"], "date": commit_data["commit"]["author"]["date"], "commits_msg": commit_data["commit"]["message"]}
+        result.append(new_dict)
+    return result
+
 
 @app.route("/githubapi", methods=["GET", "POST"])
 def githubapi():
     if request.method == "POST":
         username = request.form.get("ghubusername")
-        make_request(username)
-        return render_template("githubapi.html", username=username)
+        data = format_result(make_request(username))
+        return render_template("githubapi.html", username=username, data=data)
     return render_template("githubapi.html")
